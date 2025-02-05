d6a418aa73c0ba4ed92bade7e1ec2351448417a1
lusuijie <lusuijie@lusuijiedeMacBookPro.local>
2025-02-05 19:02:28 +0800

    feat: 打包配置

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export', // 启用静态导出
+    basePath: '', // 如果部署到子路径
+    assetPrefix: '', // 静态资源路径
+    images: {
+        unoptimized: true, // 静态导出时禁用图片优化
+    },
+};
 
 export default nextConfig;
