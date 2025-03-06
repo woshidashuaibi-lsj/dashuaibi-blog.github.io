bc133fb163699fe0d027c0115e9f84f56a160bc4
lusuijie <lusuijie@lusuijiedeMacBookPro.local>
2025-03-06 20:03:27 +0800

    feat: 修改静态资源路径

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     output: 'export', // 启用静态导出
     basePath: '', // 如果部署到子路径
-    assetPrefix: './', // 静态资源路径
+    assetPrefix: '/', // 静态资源路径
     images: {
         unoptimized: true, // 静态导出时禁用图片优化
     },
